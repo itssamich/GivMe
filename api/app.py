c27193ab5cdd2355304826d6eae2007afdce3350
Shunn Shine <jack.shunn@gmail.com>
2022-03-19 09:07:40 -0600

    Changed color to hexadecimal

--- api/app.py
+++ api/app.py
@@ -4,12 +4,13 @@ import EmbeddingHandler as eh
 app = Flask(__name__)
 
 
-@app.route("/<wordlist_name>/<dimensions_input>")
-def json_response(wordlist_name, dimensions_input):
+@app.route("/<wordlist_name>/<dimensions_input>/<k_clusters>")
+def json_response(wordlist_name, dimensions_input, k_clusters):
 
     labels_list, projected_embedding = eh.PCA(wordlist_name, number_of_components = int(dimensions_input))
-    clusters = eh.k_means_clus(projected_embedding, 3)
-    c_colors = list(map(lambda x: "black" if x == 0 else "red" if x == 1 else "green", clusters))
+    clusters = eh.k_means_clus(projected_embedding, int(k_clusters))
+    color_options = ["#00188f", "#fff100", "#00bcf2", "#ff8c00", "#e81123", "#00b294", "#ec008c", "#009e49", "#68217a", "#bad80a"]
+    c_colors = list(map(lambda x: color_options[x], clusters))
     x_coords = list(projected_embedding[:,0])
     y_coords = list(projected_embedding[:,1])
     z_coords = list(projected_embedding[:,2]) if dimensions_input == "3" else []
